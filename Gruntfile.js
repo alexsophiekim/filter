9b241937d1b9cb489e79894b152f28ba0acbe787
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2019-07-31 22:40:49 +0000

    practice

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,13 +9,26 @@ module.exports = function(grunt) {
         }
       }
     },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css/',
+          src: ['*.css', '!*.min.css'],
+          dest: 'css/',
+          ext: '.min.css'
+        }]
+      }
+    }
 
   });
 
   // Load the plugin that provides the "uglify" task.
    grunt.loadNpmTasks('grunt-contrib-sass');
+   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
    grunt.registerTask('compile', ['sass']);
+   grunt.registerTask('all', ['sass', 'cssmin']);
 
 };
---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  $.ajax({
+    type: 'GET',
+    url: `https://my.api.mockaroo.com/peopledata.json?key=0abb4470`,
+    dataType: 'json',
+    
+  })
+
+
+
+
+});
